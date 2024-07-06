b7cff7b0a1756396b8d55c86e2fe90d0f39bd1e2
yemomo <1463645455@qq.com>
2024-07-06 11:46:56 +0800

    Initial Commit

---
+++ YemomoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YemomoApp
+//
+//  Created by 叶墨沫 on 2024/7/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YemomoApp/YemomoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YemomoAppApp.swift
+//  YemomoApp
+//
+//  Created by 叶墨沫 on 2024/7/6.
+//
+
+import SwiftUI
+
+@main
+struct YemomoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
